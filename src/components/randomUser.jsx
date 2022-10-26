dd75e6ab1819b1be225b949e3ec304ad8e86e60e
İrfan Sütlü <peroxcid@haotmail.com>
2022-10-26 16:47:42 +0300

    random-user

---
+++ src/components/randomUser.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+
+const RandomUser = ()=>{
+    const url = "https://randomuser.me/api/";
+    const [person, setPerson] = useState()
+    return(
+        <p>sa</p>
+    )
+}
+export default RandomUser
